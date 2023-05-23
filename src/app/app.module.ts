2d100ba90af78663e9aa3a7d4699324164d6a7ad
Rattatammanoon Prawasuk <toprattatammanoon@hotmail.com>
2023-05-23 16:53:14 +0700

    update frontend add Form New Categories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,7 +27,9 @@ import { CategoriesComponent } from './categories/categories.component';
     BrowserModule,
     AppRoutingModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideFirestore(() => getFirestore())
+    provideFirestore(() => getFirestore()),
+
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.css']
 })
-export class CategoriesComponent {
+export class CategoriesComponent implements OnInit{
+
+  constructor(){
+
+  }
+  ngOnInit():void{}
+  onSubmit(formData: any){
+    let categoryData = {
+      category: formData.value.category
+    }
+
+    
+    
+    
+  }
+
 
 }
