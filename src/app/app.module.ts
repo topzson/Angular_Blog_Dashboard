febb0a9eaee74be345a194c01c6fa8b58eab0669
Rattatammanoon Prawasuk <toprattatammanoon@hotmail.com>
2023-05-23 13:29:42 +0700

    connect firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { environment } from '../environments/environment';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { CategoriesComponent } from './categories/categories.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'ang-blog-9ab69',
+    appId: '1:667446433523:web:8080d5f33b63fbdbb10986',
+    storageBucket: 'ang-blog-9ab69.appspot.com',
+    apiKey: 'AIzaSyDBBhKf9UfKM9UutyV3WHYsgoAr4DTplJE',
+    authDomain: 'ang-blog-9ab69.firebaseapp.com',
+    messagingSenderId: '667446433523',
+  },};
