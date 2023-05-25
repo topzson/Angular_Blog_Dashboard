3cefe7ccb68bae1bbf2f99291e81f554f78aab8f
Rattatammanoon Prawasuk <toprattatammanoon@hotmail.com>
2023-05-25 11:20:32 +0700

    issue angularFireStorage

--- src/app/posts/new-post/new-post.component.ts
+++ src/app/posts/new-post/new-post.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators } from '@angular/forms';
 import { CategoriesService } from 'src/app/services/categories.service';
 import { FormBuilder } from '@angular/forms';
+import { Post } from 'src/app/models/post';
+import { PostsService } from 'src/app/services/posts.service';
 @Component({
   selector: 'app-new-post',
   templateUrl: './new-post.component.html',
@@ -14,8 +16,8 @@ export class NewPostComponent implements OnInit{
   selectedimg: any;
 
   categories: any = [];
-  postForm: FormGroup;
-  constructor(private categoryService: CategoriesService, private fb: FormBuilder){
+  postForm: FormGroup
+  constructor(private categoryService: CategoriesService, private fb: FormBuilder, private postService: PostsService){
 
     this.postForm = this.fb.group({
       title: ['', [Validators.required, Validators.minLength(10)]],
@@ -49,5 +51,28 @@ export class NewPostComponent implements OnInit{
     }
     reader.readAsDataURL($event?.target.files[0]);
     this.selectedimg = $event.target.files[0];
+  }
+
+  onSubmit(){
+    console.log(this.postForm.value);
+    let splitted = this.postForm.value.category.split('-');
+    
+    const postData: Post = {
+      title: this.postForm.value.title,
+      permalink: this.postForm.value.permalink,
+      category: {
+        categoryId: splitted[0],
+        category:splitted[1]
+      },
+      postImgPath: '',
+      excerpt: this.postForm.value.excerpt,
+      content: this.postForm.value.content,
+      isFeatured: false,
+      views: 0,
+      status: 'new',
+      createdAt: new Date()
+    }
+
+    this.postService.uploadImage(this.selectedimg);
   }
 }
