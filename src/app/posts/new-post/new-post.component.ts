c299a56e713b6f509f53064b54d742b10f3bf431
Rattatammanoon Prawasuk <toprattatammanoon@hotmail.com>
2023-05-25 14:43:20 +0700

    add update delete markFreatured

--- src/app/posts/new-post/new-post.component.ts
+++ src/app/posts/new-post/new-post.component.ts
@@ -4,47 +4,89 @@ import { CategoriesService } from 'src/app/services/categories.service';
 import { FormBuilder } from '@angular/forms';
 import { Post } from 'src/app/models/post';
 import { PostsService } from 'src/app/services/posts.service';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-new-post',
   templateUrl: './new-post.component.html',
   styleUrls: ['./new-post.component.css']
 })
-export class NewPostComponent implements OnInit{
+export class NewPostComponent implements OnInit {
 
-  permalink: string= '';
+  permalink: string = '';
   imgSrc: any = './assets/placeholder-image.png';
   selectedimg: any;
+  post: any;
+  formStatus: string = 'Add New';
+  docId: string | undefined;
 
   categories: any = [];
   postForm: FormGroup
-  constructor(private categoryService: CategoriesService, private fb: FormBuilder, private postService: PostsService){
+  constructor(
+    private categoryService: CategoriesService,
+    private fb: FormBuilder,
+    private postService: PostsService,
+    private route: ActivatedRoute
+  ) {
+    this.route.queryParams.subscribe(val => {
+
+      this.docId = val['id'];
+
+      if (this.docId) {
+        this.postService.loadOneData(val['id']).subscribe(post => {
+          this.post = post;
+          this.postForm = this.fb.group({
+            title: [this.post.title, [Validators.required, Validators.minLength(10)]],
+            permalink: [this.post.permalink, Validators.required],
+            excerpt: [this.post.excerpt, [Validators.required, Validators.minLength(50)]],
+            category: [`${this.post.category.categoryId}-${this.post.category.category}`, Validators.required],
+            postImg: ['', Validators.required],
+            content: [this.post.content, Validators.required],
+          })
+          this.imgSrc = this.post.postImgPath;
+          this.formStatus = 'Edit';
+        });
+      }else{
+        this.postForm = this.fb.group({
+          title: ['', [Validators.required, Validators.minLength(10)]],
+          permalink: ['', Validators.required],
+          excerpt: ['', [Validators.required, Validators.minLength(50)]],
+          category: ['', Validators.required],
+          postImg: ['', Validators.required],
+          content: ['', Validators.required],
+        })
+      }
+
+
+    })
+
+
 
     this.postForm = this.fb.group({
       title: ['', [Validators.required, Validators.minLength(10)]],
       permalink: ['', Validators.required],
       excerpt: ['', [Validators.required, Validators.minLength(50)]],
-      category:['', Validators.required],
-      postImg:['', Validators.required],
-      content:['' , Validators.required],
+      category: ['', Validators.required],
+      postImg: ['', Validators.required],
+      content: ['', Validators.required],
     })
   }
   ngOnInit(): void {
-      this.categoryService.loadData().subscribe(val => {
-        this.categories = val;
-      })
+    this.categoryService.loadData().subscribe(val => {
+      this.categories = val;
+    })
   }
 
-  get fc(){
+  get fc() {
     return this.postForm.controls
   }
 
-  onTitleChanged($event: any){
+  onTitleChanged($event: any) {
     const title = $event.target.value;
-    this.permalink = title.replace(/\s/g,'-');
+    this.permalink = title.replace(/\s/g, '-');
 
   }
 
-  showPreview($event:any){
+  showPreview($event: any) {
     const reader = new FileReader();
     reader.onload = (e) => {
       this.imgSrc = e.target?.result
@@ -53,16 +95,16 @@ export class NewPostComponent implements OnInit{
     this.selectedimg = $event.target.files[0];
   }
 
-  onSubmit(){
+  onSubmit() {
     console.log(this.postForm.value);
     let splitted = this.postForm.value.category.split('-');
-    
+
     const postData: Post = {
       title: this.postForm.value.title,
       permalink: this.postForm.value.permalink,
       category: {
         categoryId: splitted[0],
-        category:splitted[1]
+        category: splitted[1]
       },
       postImgPath: '',
       excerpt: this.postForm.value.excerpt,
@@ -73,6 +115,8 @@ export class NewPostComponent implements OnInit{
       createdAt: new Date()
     }
 
-    this.postService.uploadImage(this.selectedimg , postData);
+    this.postService.uploadImage(this.selectedimg, postData, this.formStatus, this.docId);
+    this.postForm.reset();
+    this.imgSrc = './assets/placeholder-image.png';
   }
 }
--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -3,6 +3,10 @@ import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { AngularFireDatabase, AngularFireList } from '@angular/fire/compat/database';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { ToastrService } from 'ngx-toastr';
+import { Params, Router } from '@angular/router';
+
+import { map } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,9 +14,10 @@ export class PostsService {
 
   constructor(private storage: AngularFireStorage,
     private afs: AngularFirestore,
-    private toastr: ToastrService) { }
+    private toastr: ToastrService,
+    private router: Router) { }
 
-  uploadImage(selectedImage: any,postData :any){
+  uploadImage(selectedImage: any,postData :any, formStatus:any, id:any){
     const filePath = `postIMG/${Date.now()}`;
     console.log(filePath);
 
@@ -21,17 +26,72 @@ export class PostsService {
       this.storage.ref(filePath).getDownloadURL().subscribe(URL =>{
         postData.postImgPath = URL;
         console.log(postData);
-
-        this.afs.collection('posts').add(postData).then(docRef =>{
-          this.toastr.success('Data Insert Successfully');
+        if(formStatus == 'Edit'){
+          this.updateData(id, postData)
+        }else{
+          this.saveData(postData);
+        }
+        
         })
 
       })
 
+    
+  }
+
+  saveData(postData :any){
+    this.afs.collection('posts').add(postData).then(docRef =>{
+      this.toastr.success('Data Insert Successfully');
+      this.router.navigate(['./posts']);
+    });
+  }
+
+  loadData(){
+
+    return this.afs.collection('posts').snapshotChanges().pipe(
+      map(actions => {
+        return actions.map(a => {
+          
+          const data = a.payload.doc.data();
+          const id = a.payload.doc.id;
+          return { id, data}
+
+        })
+      })
+    )
+
+  }
+
+  loadOneData(id: Params){
+    return this.afs.doc(`posts/${id}`).valueChanges();
+
+  }
+
+  updateData(id: any,postData:any){
+    this.afs.doc(`posts/${id}`).update(postData).then(()=>{
+      this.toastr.success('Data Updated Successfully');
+      this.router.navigate(['/posts']);
+    })
+
+  }
+  deleteImage(postImgPath: any ,id:any){
+
+    this.storage.storage.refFromURL(postImgPath).delete().then(() =>{
+      this.deleteData(id);
+
     })
   }
 
-  saveData(){
-    
+  deleteData(id:any){
+    this.afs.doc(`posts/${id}`).delete().then(()=>{
+      this.toastr.warning('Data Deleted ..!');
+
+    })
+  }
+  markFeatured(id:any, featuredData:any){
+
+    this.afs.doc(`posts/${id}`).update(featuredData).then(()=>{
+      this.toastr.info('Featured Status Update');
+    })
   }
 }
