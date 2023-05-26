4808123f4ba332d3c40d3f086afb8a912a1e98e1
Rattatammanoon Prawasuk <toprattatammanoon@hotmail.com>
2023-05-26 10:36:00 +0700

    end Authentication& Authorization

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
-  
+export class HeaderComponent implements OnInit {
+
+  userEmail: string | undefined;
+  isLoggedIn$: Observable<boolean> | undefined
+  constructor(private authService: AuthService){
+
+  }
+
+  ngOnInit(): void {
+    const storedUser = localStorage.getItem('user');
+    if (storedUser !== null) {
+      const user = JSON.parse(storedUser);
+      this.userEmail = user.email;
+    }
+    this.isLoggedIn$ = this.authService.isLoggedIn();
+
+    
+  }
+
+  onlogOut(){
+    this.authService.logOut();
+  }
 }
